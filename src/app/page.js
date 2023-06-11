bbf7b40cfa6d191124cf88053474283f7ee08548
Shivam Akhouri <shivamakhouri1142@gmail.com>
2023-06-12 01:02:44 +0530

    Search Field

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Button from '@/components/Button'
 import IconButton from '@/components/IconButton'
 import InboxCard from '@/components/InboxCard'
+import InputField from '@/components/InputField'
 import Image from 'next/image'
-import {FaPlus} from 'react-icons/fa'
+import {FaPlus, FaSearch} from 'react-icons/fa'
 
 export default function Home() {
   return (
     <main>
-      <InboxCard />
+      <InputField foreImage={<FaSearch size={18} />}/>
     </main>
   )
 }
--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,7 +1,12 @@
 
 
-export default function Button(props){
+export default function InputField(props){
     return (
-        <div></div>
+        <div className="flex w-full py-3 bg-[#0c0c0c] rounded-md">
+            <div className="ml-4 mr-4">
+            {props.foreImage}
+            </div>
+            <input type="text" placeholder="Search" className="w-full h-full bg-t"/>
+        </div>
     );
 }
