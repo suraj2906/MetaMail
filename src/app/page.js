5f81468d5f8f2d8e2fbdcfead6613a74ac10b92d
Shivam Akhouri <shivamakhouri1142@gmail.com>
2023-06-12 16:45:44 +0530

    Tab bar added

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,14 @@ import IconButton from '@/components/IconButton'
 import InboxCard from '@/components/InboxCard'
 import InputField from '@/components/InputField'
 import NavItem from '@/components/NavItem'
+import Tab from '@/components/Tab'
 import Image from 'next/image'
 import {FaPlus, FaSearch} from 'react-icons/fa'
 
 export default function Home() {
   return (
     <main>
-      <NavItem foreImage={<FaPlus className='text-purple' size={21} />}>
-        About
-      </NavItem>
-      <NavItem foreImage={<FaPlus className='text-purple' size={21} />}>
-        About
-      </NavItem>
-      <NavItem foreImage={<FaPlus className='text-purple' size={21} />}>
-        About
-      </NavItem>
+      <Tab />
     </main>
   )
 }
